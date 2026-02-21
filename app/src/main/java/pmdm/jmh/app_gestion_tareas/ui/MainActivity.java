3fa9c4db95a789654afcbb41101646bc026a67a9
jamarten291 <146338884+jamarten291@users.noreply.github.com>
2026-02-21 11:54:30 +0100

    add animations in main activity

--- app/src/main/java/pmdm/jmh/app_gestion_tareas/ui/MainActivity.java
+++ app/src/main/java/pmdm/jmh/app_gestion_tareas/ui/MainActivity.java
@@ -2,7 +2,11 @@ package pmdm.jmh.app_gestion_tareas.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,12 +14,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 import pmdm.jmh.app_gestion_tareas.R;
 import pmdm.jmh.app_gestion_tareas.ui.tarea.list.ListadoTareasActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btEntrar;
+    private ImageView logo;
+    private TextView eslogan;
+    private final Executor executor = Executors.newSingleThreadExecutor();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +40,12 @@ public class MainActivity extends AppCompatActivity {
 
         // Bindings
         btEntrar = findViewById(R.id.bt_entrar);
+        logo = findViewById(R.id.iv_logo);
+        eslogan = findViewById(R.id.tv_slogan);
+
+        // Animaciones
+        Animation logoAnimation = AnimationUtils.loadAnimation(this, R.anim.scale_up_down);
+        Animation sloganAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
 
         // Listener
         btEntrar.setOnClickListener(v -> {
@@ -37,5 +53,11 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
             finish();
         });
+
+        // Executor que lanza las animaciones
+        executor.execute(() -> runOnUiThread(() -> {
+            logo.startAnimation(logoAnimation);
+            eslogan.startAnimation(sloganAnimation);
+        }));
     }
 }
